09e432a15aa9f8ab07855bc09b9b61b38e891d5b
ankitpawar1 <ankitpawar192837@gmail.com>
2024-09-25 11:36:12 -0700

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import FlameGame from "./FlameGame";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <h1>FLAMES Game</h1>
+        <FlameGame />
+      </div>
+    );
+  }
 }
-
 
 export default App;
